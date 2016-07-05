865a1b0e91ba4884c033d3a41ab0b39fc5ae9898
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-07-04 21:37:23 -0400

    init

---
+++ dfm.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/chasinglogic/dfm/commands"
+	"github.com/urfave/cli"
+)
+
+func defaultConfigDir() string {
+	xdg := os.Getenv("XDG_CONFIG_HOME")
+
+	if xdg == "" {
+		xdg = filepath.Join(os.Getenv("HOME"), ".config")
+	}
+
+	return filepath.Join(xdg, "dfm")
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "dfm"
+	app.Usage = "Manage dotfiles."
+	app.Version = "1.0-dev"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "config, c",
+			Usage: "Use `DIR` for storing dfm configuration",
+			Value: defaultConfigDir(),
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "create",
+			Aliases: []string{"c"},
+			Usage:   "Create a dotfiles profile from a git repo.",
+			Action:  commands.Create,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "alias, a",
+					Usage: "Creates `ALIAS` for the profile instead of username",
+				},
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
