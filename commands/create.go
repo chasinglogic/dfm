2667ece312721ecffb69724bcdb81eb6cddb4ca5
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-07-05 11:29:11 -0400

    better error message

--- commands/create.go
+++ commands/create.go
@@ -61,7 +61,7 @@ func cloneRepo(url, user, userDir string) error {
 	c := exec.Command("git", "clone", url, userDir)
 	_, err := c.CombinedOutput()
 	if err != nil && err.Error() == "exit status 128" {
-		return cli.NewExitError("Profile exists, perhaps you meant dfm update?", 128)
+		return cli.NewExitError("Profile exists, perhaps you meant dfm update or link?", 128)
 	}
 
 	return err
