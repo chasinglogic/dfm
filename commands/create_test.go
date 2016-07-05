fb3530aae8a0e77e138c0daabf52a997ccefa29d
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-07-04 21:37:57 -0400

    unit tests where relevant

---
+++ commands/create_test.go
@@ -0,0 +1,28 @@
+package commands
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestCreateLongURL(t *testing.T) {
+	url, user := createURL(strings.Split("bitbucket.org/chasinglogic/dotfiles", "/"))
+	if url != "https://bitbucket.org/chasinglogic/dotfiles" {
+		t.Errorf("Expected: %s Got: %s", "https://bitbucket.org/chasinglogic/dotfiles", url)
+	}
+
+	if user != "chasinglogic" {
+		t.Errorf("Expected: %s Got: %s", "chasinglogic", user)
+	}
+}
+
+func TestCreateShortURL(t *testing.T) {
+	url, user := createURL(strings.Split("chasinglogic/dotfiles", "/"))
+	if url != "https://github.com/chasinglogic/dotfiles" {
+		t.Errorf("Expected: %s Got: %s", "https://github.com/chasinglogic/dotfiles", url)
+	}
+
+	if user != "chasinglogic" {
+		t.Errorf("Expected: %s Got: %s", "chasinglogic", user)
+	}
+}
