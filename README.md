98b2354348112f414fb7add483788acf9528b8c1
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-28 17:10:35 -0400

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+# dfm
+A dotfile manager for pair programmers.
+
+Dotfile Manager will allow you to create "profiles" for dotfiles underneath one
+unix account and easily switch between them. It requires a git repo with your
+dotfiles in it and that the dotfiles be placed how you want them represented in
+your home directory.
+
+## Installation
+The easiest (and currently only) way to install dfm is using pip
+
+```bash
+pip install dfm
+```
+
+## Usage
+
+TODO: Write usage docs
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. :fire: Submit a pull request :D :fire:
+
+All pull requests should go to the develop branch not master. Thanks!
+
+## License
+
+dfm is distributed under the GPLv3
+
+```
+
+```
---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+A dotfile manager for pair programmers.
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='dfm',
+    version='0.1',
+    url='https://github.com/chasinglogic/dfm',
+    license='GPLv3',
+    author='Mathew Robinson',
+    author_email='mathew.robinson3114@gmail.com',
+    description='A dotfile manager for pair programmers.',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'dfm = dfm.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
