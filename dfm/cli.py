d499ae2b15df2578eb9bb31b96cbc8399d265155
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-09-30 14:48:41 -0400

    fix setup.py and version bump

--- dfm/cli.py
+++ dfm/cli.py
@@ -43,7 +43,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-VERSION_NUMBER = 0.1
+VERSION_NUMBER = 0.2
 
 @click.group()
 @click.option("--verbose", "-vv",
