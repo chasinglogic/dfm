367150e6c3240ec62dbf5c8a210e98aed4c9a898
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-10-20 10:51:18 -0400

    renamed to clone

--- create.go
+++ create.go
@@ -12,7 +12,7 @@ import (
 
 // Create will clone the given git repo to the profiles directory, it optionally
 // will call link or use depending on the flag given.
-func Create(c *cli.Context) error {
+func Clone(c *cli.Context) error {
 	var aliasDir string
 
 	if alias := c.String("alias"); alias != "" {
