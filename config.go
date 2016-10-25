d1d193a517999a0ff3fb4403e00e63131366f3fa
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-10-25 09:48:54 -0400

    godoc

--- config.go
+++ config.go
@@ -9,16 +9,19 @@ import (
 	cli "gopkg.in/urfave/cli.v1"
 )
 
+// Config holds some basic information about the state of dfm.
 type Config struct {
 	Verbose        bool
 	ConfigDir      string
 	CurrentProfile string
 }
 
+// LoadConfig runs as a hook when dfm runs to load the config object from
+// ~/.dfm
 func LoadConfig(c *cli.Context) error {
 	configJSON, rerr := ioutil.ReadFile(filepath.Join(os.Getenv("HOME"), ".dfm"))
 	if rerr == os.ErrNotExist {
-		configJSON = []byte("{\"Verbose\":false}")
+		configJSON = []byte("{\"CurrentProfile\":false}")
 	} else if rerr != nil {
 		return rerr
 	}
@@ -44,6 +47,7 @@ func LoadConfig(c *cli.Context) error {
 
 // SaveConfig will save the config to the configDir/config.json
 func SaveConfig(c *cli.Context) error {
+	CONFIG.Verbose = false
 	JSON, merr := json.MarshalIndent(CONFIG, "", "\t")
 	if merr != nil {
 		return merr
@@ -56,6 +60,8 @@ func getProfileDir() string {
 	return filepath.Join(CONFIG.ConfigDir, "profiles")
 }
 
+// DefaultConfigDir will return the default location to store profiles which is
+// $XDG_CONFIG_HOME/dfm or $HOME/.config/dfm
 func DefaultConfigDir() string {
 	xdg := os.Getenv("XDG_CONFIG_HOME")
 
