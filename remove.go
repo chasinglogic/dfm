5ec1af8892ab2eea8eda1d31a750099e31e7991b
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-10-25 09:49:45 -0400

    wrote GenerateSymlinks since changes to link.go

--- remove.go
+++ remove.go
@@ -2,6 +2,7 @@ package dfm
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -9,6 +10,7 @@ import (
 	cli "gopkg.in/urfave/cli.v1"
 )
 
+// Remove will remove the specified profile
 func Remove(c *cli.Context) error {
 	profile := c.Args().First()
 	if profile == "" {
@@ -16,7 +18,7 @@ func Remove(c *cli.Context) error {
 	}
 
 	userDir := filepath.Join(getProfileDir(), profile)
-	links := GenerateSymlinks(userDir)
+	links := GenerateSymlinks(userDir, os.Getenv("HOME"))
 
 	rmerr := os.RemoveAll(userDir)
 	if rmerr != nil {
@@ -30,12 +32,43 @@ func Remove(c *cli.Context) error {
 	return RemoveSymlinks(links, c.Args().First())
 }
 
-func RemoveSymlinks(l []LinkInfo, username string) error {
-	for _, link := range l {
+// GenerateSymlinks will create the symlinks so we know what they were supposed
+// to be prior to removing the profile.
+func GenerateSymlinks(profileDir, target string) *[]LinkInfo {
+	var lnks []LinkInfo
+
+	files, err := ioutil.ReadDir(profileDir)
+	if err != nil {
+		fmt.Println(err)
+		return &lnks
+	}
+
+	for _, file := range files {
+		// Handle the XDG_CONFIG_HOME special case
+		if file.Name() == "config" && file.IsDir() {
+			xdg := os.Getenv("XDG_CONFIG_HOME")
+			if xdg == "" {
+				xdg = filepath.Join(os.Getenv("HOME"), ".config")
+			}
+
+			lnks = append(lnks,
+				*GenerateSymlinks(filepath.Join(profileDir, file.Name()), xdg)...)
+		}
+
+		lnks = append(lnks, *GenerateSymlink(profileDir, target, file))
+	}
+
+	return &lnks
+}
+
+// RemoveSymlinks will remove all of the symlinks after removing a profile it
+// will first Check if the link is still valid after removing the profile, and
+// if so just verify that it doesn't contain the username of the profile
+// we're deleting. If the profile we're removing is the one that was currently
+// in use then both conditions should be true.
+func RemoveSymlinks(l *[]LinkInfo, username string) error {
+	for _, link := range *l {
 		// Check if the link is still valid after removing the profile, and if
-		// so just verify thta it doesn't contain the username of the profile
-		// we're deleting. If the profile we're removing is the one that was
-		// currently in use then both conditions should be true.
 		if path, err := os.Readlink(link.Dest); err != nil ||
 			strings.Contains(path, username) {
 
@@ -49,6 +82,7 @@ func RemoveSymlinks(l []LinkInfo, username string) error {
 				}
 			}
 		}
+
 	}
 
 	return nil
