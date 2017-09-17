207ddade3c87245b226640a4de02e317426aeccf
Mathew Robinson <chasinglogic@gmail.com>
2017-09-17 16:30:35 -0400

    Update license

--- cli/cli.go
+++ cli/cli.go
@@ -57,6 +57,35 @@ func buildApp() *cli.App {
 
 	app.Commands = []cli.Command{
 		{
+			Name:  "license",
+			Usage: "Print version and licensing info about dfm.",
+			Action: func(c *cli.Context) error {
+				fmt.Printf(`Name:
+    dfm - Manage dotfiles.
+
+Version:
+    %s
+
+License:
+    Copyright (C) 2017 Mathew Robinson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+`, c.App.Version)
+				return nil
+			},
+		},
+		{
 			Name:    "add",
 			Aliases: []string{"a"},
 			Usage:   "Add a file to the current profile.",
