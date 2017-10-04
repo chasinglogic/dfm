29bacbcf1a10535eb3d3b08faef354df5cd7e1f9
Mathew Robinson <chasinglogic@gmail.com>
2017-10-03 23:54:54 -0400

    Add License Headers

--- backend/dropbox/dropbox.go
+++ backend/dropbox/dropbox.go
@@ -1,3 +1,7 @@
+// Copyright 2017 Mathew Robinson <mrobinson@praelatus.io>. All rights reserved.
+// Use of this source code is governed by the GPLv3 license that can be found in
+// the LICENSE file.
+
 package dropbox
 
 import (
