74e15ee2c5518f59c11e18fda18b288685fbe70a
Mathew Robinson <chasinglogic@gmail.com>
2017-12-11 14:31:12 -0500

    Export hook functions and types for use in run hook command

---
+++ commands/run_hook.go
@@ -0,0 +1,29 @@
+// Copyright 2017 Mathew Robinson <mrobinson@praelatus.io>. All rights reserved.
+// Use of this source code is governed by the GPLv3 license that can be found in
+// the LICENSE file.
+
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/chasinglogic/dfm/hooks"
+	"github.com/spf13/cobra"
+)
+
+// RunHook will add the specified profile to the current profile, linking it as
+// necessary.
+var RunHook = &cobra.Command{
+	Use:   "run-hook",
+	Short: "Run the hook specified by `HOOK`",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("ERROR: Must specify at least one hook to run")
+			os.Exit(128)
+		}
+
+		hookYaml := hooks.Load()
+		hooks.RunCommands(hookYaml[args[0]])
+	},
+}
