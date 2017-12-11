680e46cede1c0e02962d5c219ea31bfbb4073a1f
Mathew Robinson <chasinglogic@gmail.com>
2017-12-11 14:48:27 -0500

    Update copyright

--- commands/sync.go
+++ commands/sync.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Mathew Robinson <mrobinson@praelatus.io>. All rights reserved.
+// Copyright 2017 Mathew Robinson <chasinglogic@gmail.com>. All rights reserved.
 // Use of this source code is governed by the GPLv3 license that can be found in
 // the LICENSE file.
 
