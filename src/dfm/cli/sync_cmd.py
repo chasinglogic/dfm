ddb84fc80d73df4033ace52d036dd2d932d5825b
Mathew Robinson <chasinglogic@gmail.com>
2019-03-14 09:24:08 -0400

    Add --message flag to sync command

--- src/dfm/cli/sync_cmd.py
+++ src/dfm/cli/sync_cmd.py
@@ -1,6 +1,9 @@
 """
 Usage: dfm sync [options]
 
+Options:
+    -m, --message <msg>  Use the given <msg> as the commit message.
+
 Sync the current profile and modules.
 """
 
@@ -8,6 +11,8 @@ from dfm.cli.utils import inject_profile
 
 
 @inject_profile
-def run(_args, profile=None):
+def run(args, profile=None):
     """Run profile.sync for the current profile."""
+    if args['--message']:
+        profile.commit_msg = args['--message']
     profile.sync()
