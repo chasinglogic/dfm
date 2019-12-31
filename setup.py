46bba681bf4b0769ab4deb41d4f9c2c6a824b11c
Mathew Robinson <chasinglogic@gmail.com>
2019-12-31 07:02:54 -0500

    Update to 7.4.0

--- setup.py
+++ setup.py
@@ -2,41 +2,38 @@
 
 from setuptools import find_packages, setup
 
-with open('README.md') as f:
+with open("README.md") as f:
     LONG_DESCRIPTION = f.read()
 
 setup(
-    name='dfm',
-    version='7.3.1',
-    url='https://github.com/chasinglogic/dfm',
-    license='GPL-3.0',
-    author='Mathew Robinson',
-    author_email='chasinglogic@gmail.com',
-    description='A dotfile manager for pair-programmers and lazy people.',
+    name="dfm",
+    version="7.4.0",
+    url="https://github.com/chasinglogic/dfm",
+    license="GPL-3.0",
+    author="Mathew Robinson",
+    author_email="chasinglogic@gmail.com",
+    description="A dotfile manager for pair-programmers and lazy people.",
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    platforms='any',
-    install_requires=['docopt', 'pyyaml>=3.13'],
-    entry_points={
-        'console_scripts': [
-            'dfm = dfm.cli:main',
-        ],
-    },
+    platforms="any",
+    install_requires=["docopt", "pyyaml>=3.13"],
+    entry_points={"console_scripts": ["dfm = dfm.cli:main",],},
     classifiers=[
         # As from https://pypi.org/classifiers/
-        'Development Status :: 4 - Beta',
+        "Development Status :: 4 - Beta",
         # 'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Affero General Public License v3',
-        'Operating System :: POSIX',
-        'Operating System :: MacOS',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ])
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
