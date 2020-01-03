80a556a410e331d2732964df75a115ec8cce6725
Mathew Robinson <chasinglogic@gmail.com>
2020-01-03 12:35:25 -0500

    Allow clone to be used with --name flag. Resolves #48

--- src/dfm/cli/clone_cmd.py
+++ src/dfm/cli/clone_cmd.py
@@ -14,6 +14,7 @@ Options:
 Clones the repository at <url> to a new profile with <name>
 """
 
+import sys
 import os
 import subprocess
 
@@ -25,15 +26,38 @@ def get_name(url):
     """
     Generate a profile name based on the git url.
 
-    This directly corresponds to the the last element in the URL.  For
-    example: https://github.com/chasinglogic/dotfiles would be 'dotfiles'
+    This directly corresponds to the second to last element in the URL.  For
+    example: https://github.com/chasinglogic/dotfiles would be 'chasinglogic'
+
+    In the case of an ssh url or other url will correspond to the
+    first path element. For example:
+    git@github.com:chasinglogic/dotfiles would be 'chasinglogic'
     """
-    return url.split("/")[-1]
+    try:
+        if url.find("@") > 1:
+            return url.split(":")[-1].split("/")[0]
+        else:
+            return url.split("/")[-2]
+    # Any kind of exception i.e. IndexError or failure to split we
+    # just return nothing.
+    except Exception:
+        return ""
 
 
 def run(args):
     """Run the clone command."""
-    name = args.get("--name", get_name(args["<url>"]))
+    name = args["--name"]
+    if name is None:
+        name = get_name(args["<url>"])
+
+    if not name:
+        print(
+            "--name flag not provided and could not be determined from",
+            args["<url>"],
+            "please provide a name for this profile via the --name flag",
+        )
+        sys.exit(1)
+
     path = os.path.join(dfm_dir(), "profiles", name)
     subprocess.call(["git", "clone", args["<url>"], path])
     if args["--link"]:
---
+++ tests/test_url_get_name.py
@@ -0,0 +1,16 @@
+"""Test automatic name detection for URLs."""
+
+
+from dfm.cli.clone_cmd import get_name
+
+
+def test_get_name():
+    test_cases = [
+        ("https://github.com/chasinglogic/dotfiles", "chasinglogic"),
+        ("http://github.com/chasinglogic/dotfiles", "chasinglogic"),
+        ("git@github.com:chasinglogic/dotfiles", "chasinglogic"),
+        ("keybase://private/chasinglogic/dotfiles", "chasinglogic"),
+    ]
+
+    for url, expected in test_cases:
+        assert get_name(url) == expected
