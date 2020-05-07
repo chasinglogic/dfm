8e5618c211268676943d000b61fade6f73f7fa78
Mathew Robinson <mathew@chasinglogic.io>
2020-05-07 11:16:59 +0100

    Add a --name flag to Sync
    Now that Modules and Profiles are the same thing we should let you
    selectively sync them. If the --name flag is given to sync it will
    only sync the profile with the name, if no profile with that name is
    found then it will recursively search modules from the current profile
    until it finds a module that matches the --name value.

--- src/dfm/cli/sync_cmd.py
+++ src/dfm/cli/sync_cmd.py
@@ -2,17 +2,48 @@
 Usage: dfm sync [options]
 
 Options:
-    -m, --message <msg>  Use the given <msg> as the commit message.
+    -m, --message <msg>   Use the given <msg> as the commit message.
+    -n, --name <name>     Sync only the given profile or module by name.
 
 Sync the current profile and modules.
 """
 
-from dfm.cli.utils import inject_profile
+from sys import exit
+from os.path import exists
+from dfm.cli.utils import load_profile, profile_dir
+from dfm.profile import Profile
 
 
-@inject_profile
-def run(args, profile=None):
+def find_module(name, profile):
+    """Recursively search profile and it's modules for a module with name."""
+    for mod in profile.modules:
+        if mod.name == name:
+            return mod
+
+        if mod.modules:
+            found = find_module(name, mod)
+            if found is not None:
+                return found
+
+    return None
+
+
+def run(args):
     """Run profile.sync for the current profile."""
+    if args["--name"]:
+        possible_dir = profile_dir(args["--name"])
+        if not exists(possible_dir):
+            curprofile = load_profile()
+            profile = find_module(args["--name"], curprofile)
+            if profile is None:
+                print("no module or profile matched name: {}".format(args["--name"]))
+                exit(1)
+        else:
+            profile = Profile(possible_dir)
+    else:
+        profile = load_profile()
+
     if args["--message"]:
         profile.commit_msg = args["--message"]
+
     profile.sync()
--- src/dfm/cli/utils.py
+++ src/dfm/cli/utils.py
@@ -4,7 +4,7 @@ import json
 import os
 import sys
 
-from dfm.dotfile import Profile, dfm_dir
+from dfm.profile import Profile, dfm_dir
 
 
 def state_file_p():
@@ -12,34 +12,8 @@ def state_file_p():
     return os.path.join(dfm_dir(), "state.json")
 
 
-def load_profile(name):
-    """
-    Load a profile by name.
-
-    Joins the dfm state directory with 'profiles' and name to
-    determine where the profile is.
-    """
-    path = os.path.join(dfm_dir(), "profiles", name)
-    return Profile(path)
-
-
-def switch_profile(name):
-    """
-    Switch profile will update the state file to the profile with name.
-
-    Returns the profile object as returned by load_profile.
-    """
-    path = state_file_p()
-    with open(path, "w+") as state_file:
-        content = state_file.read()
-        if content:
-            state = json.loads(content)
-        else:
-            state = {}
-
-        state["current_profile"] = name
-        json.dump(state, state_file)
-        return load_profile(name)
+def profile_dir(name):
+    return os.path.join(dfm_dir(), "profiles", name)
 
 
 def current_profile():
@@ -60,7 +34,37 @@ def current_profile():
         print("no profile active, run dfm link to make one active")
         sys.exit(1)
 
-    return os.path.join(dfm_dir(), "profiles", profile_name)
+    return profile_dir(profile_name)
+
+
+def load_profile(name=current_profile()):
+    """
+    Load a profile by name.
+
+    Joins the dfm state directory with 'profiles' and name to
+    determine where the profile is.
+    """
+    path = profile_dir(name)
+    return Profile(path)
+
+
+def switch_profile(name):
+    """
+    Switch profile will update the state file to the profile with name.
+
+    Returns the profile object as returned by load_profile.
+    """
+    path = state_file_p()
+    with open(path, "w+") as state_file:
+        content = state_file.read()
+        if content:
+            state = json.loads(content)
+        else:
+            state = {}
+
+        state["current_profile"] = name
+        json.dump(state, state_file)
+        return load_profile(name)
 
 
 def inject_profile(wrapped):
