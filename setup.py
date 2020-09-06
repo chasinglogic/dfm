75f5ebbdc6748d23d6bfdc8ca399838a70200527
Mathew Robinson <mathew@chasinglogic.io>
2020-09-06 14:51:42 +0100

    bump 8.2.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as f:
 
 setup(
     name="dfm",
-    version="8.2.1",
+    version="8.2.2",
     url="https://github.com/chasinglogic/dfm",
     license="GPL-3.0",
     author="Mathew Robinson",
