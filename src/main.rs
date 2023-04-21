73f96ce5536312f163f6513a821fca3df690d71c
Mathew Robinson <mathew@chasinglogic.io>
2023-04-21 09:03:01 +0100

    chore: Implement base profile and DFM config structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+mod profiles;
+
+use std::ffi::OsString;
+
+use clap::{command, Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "dfm")]
+#[command(about = "A dotfile manager for pair programmers and lazy people.", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(external_subcommand)]
+    External(Vec<OsString>),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::External(args) => {
+            let plugin_name = format!("dfm-{}", args[0].to_str().unwrap_or_default());
+            println!("Calling out to {:?} with {:?}", plugin_name, &args[1..]);
+        }
+    }
 }
