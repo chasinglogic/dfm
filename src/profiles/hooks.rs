1ecff0b1939f787ddf8ab867a47cdcde868b0444
Mathew Robinson <mathew@chasinglogic.io>
2023-04-21 21:45:06 +0100

    feat: Add init command

--- src/profiles/hooks.rs
+++ src/profiles/hooks.rs
@@ -2,20 +2,20 @@ use std::collections::HashMap;
 
 use serde;
 
-#[derive(Debug, Clone, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
 pub struct HookDefinition {
     interpreter: String,
     script: String,
 }
 
-#[derive(Debug, Clone, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
 #[serde(untagged)]
 pub enum Hook {
     String(String),
     HookDefinition(HookDefinition),
 }
 
-#[derive(Debug, Clone, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
 pub struct Hooks(HashMap<String, Vec<Hook>>);
 
 impl Hooks {
