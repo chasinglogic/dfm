19f6ea8dcb2722463ef8104e96e23a19d132f2f8
Mathew Robinson <mathew@chasinglogic.io>
2023-04-22 15:04:21 +0100

    chore: cleanup visibility, separate config to own module

--- src/profiles/profile.rs
+++ src/profiles/profile.rs
@@ -2,103 +2,20 @@ use std::{
     env,
     ffi::OsStr,
     fs::{self, File},
-    io::{self, BufReader, Write},
+    io::{self, Write},
     os,
     path::{Path, PathBuf},
     process::{Command, ExitStatus},
     str::FromStr,
 };
 
-use serde;
+use super::config::DFMConfig;
+
 use walkdir::{DirEntry, WalkDir};
-
-use super::hooks::Hooks;
-
-#[derive(Debug, Clone, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
-#[serde(rename_all = "snake_case")]
-enum LinkMode {
-    Pre,
-    Post,
-    None,
-}
-
-impl Default for LinkMode {
-    fn default() -> Self {
-        LinkMode::Post
-    }
-}
-
-fn default_off() -> bool {
-    false
-}
-
-#[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
-pub struct DFMConfig {
-    #[serde(default)]
-    pub location: String,
-    #[serde(default = "Hooks::new")]
-    pub hooks: Hooks,
-
-    #[serde(default = "default_off")]
-    prompt_for_commit_message: bool,
-    #[serde(default = "default_off")]
-    pull_only: bool,
-    #[serde(default)]
-    link: LinkMode,
-    #[serde(default = "Vec::new")]
-    modules: Vec<DFMConfig>,
-}
-
-impl Default for DFMConfig {
-    fn default() -> Self {
-        DFMConfig {
-            prompt_for_commit_message: false,
-            pull_only: false,
-            link: LinkMode::default(),
-            location: "".to_string(),
-            hooks: Hooks::new(),
-            modules: Vec::new(),
-        }
-    }
-}
-
-impl DFMConfig {
-    fn load(file: &Path) -> DFMConfig {
-        let fh = File::open(file).expect(
-            format!(
-                "Unexpected error reading {}",
-                file.to_str().unwrap_or(".dfm.yml")
-            )
-            .as_str(),
-        );
-        let reader = BufReader::new(fh);
-        let mut config: DFMConfig = serde_yaml::from_reader(reader).expect("Malformed .dfm.yml");
-        if config.location == "" {
-            config.location = file
-                .parent()
-                .expect("Unexpected error getting profile location!")
-                .to_str()
-                .expect("Unexpected error turning profile location to a string!")
-                .to_string();
-        }
-
-        for module in &mut config.modules {
-            module.expand();
-        }
-
-        config
-    }
-
-    fn expand(&mut self) {
-        if self.location.starts_with("~") {
-            self.location = shellexpand::tilde(&self.location).to_string();
-        }
-    }
-}
 
 #[derive(Debug)]
 pub struct Profile {
-    pub config: DFMConfig,
+    config: DFMConfig,
 
     location: PathBuf,
     modules: Vec<Profile>,
@@ -286,5 +203,9 @@ impl Profile {
 
     pub fn run_hook(&self, hook_name: &str) -> Result<(), io::Error> {
         self.config.hooks.run_hook(hook_name, &self.location)
+    }
+
+    pub fn get_location(&self) -> PathBuf {
+        self.location.clone()
     }
 }
