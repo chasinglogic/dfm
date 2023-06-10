ae17f6a27824c32c39420b9b9ffbe1807db3add5
Mathew Robinson <mathew@chasinglogic.io>
2023-06-10 11:42:39 +0100

    feat: Write clone command

--- cmd/clone.go
+++ cmd/clone.go
@@ -2,7 +2,12 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
 
+	"github.com/chasinglogic/dfm/profiles"
 	"github.com/spf13/cobra"
 )
 
@@ -13,8 +18,57 @@ var (
 var cloneCmd = &cobra.Command{
 	Use:   "clone",
 	Short: "Clone a git repo as a dotfile profile",
+	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Println("TODO")
+		cloneUrl := args[0]
+		if !strings.HasPrefix(cloneUrl, "http") && !strings.HasPrefix(cloneUrl, "git") {
+			if !strings.Contains(cloneUrl, "/") {
+				cloneUrl = fmt.Sprintf("%s/dotfiles", cloneUrl)
+			}
+
+			cloneUrl = fmt.Sprintf("https://github.com/%s", cloneUrl)
+		}
+
+		var name string
+
+		if strings.HasPrefix(cloneUrl, "http") {
+			split := strings.Split(cloneUrl, "/")
+			name = split[len(split)-2]
+		} else {
+			split := strings.Split(cloneUrl, ":")
+			split = strings.Split(split[1], "/")
+			name = split[len(split)-2]
+		}
+
+		targetPath := path.Join(profiles.ProfileDir, name)
+
+		fmt.Println("cloning profile", cloneUrl, "as", name)
+		git := exec.Command("git", "clone", cloneUrl, targetPath)
+		git.Stdin = os.Stdin
+		git.Stdout = os.Stdout
+		git.Stderr = os.Stderr
+		err := git.Wait()
+		if err != nil {
+			return err
+		}
+
+		if !link {
+			return nil
+		}
+
+		profile, err := loadProfileByName(name)
+		if err != nil {
+			return err
+		}
+
+		err = profile.Link(profiles.LinkOptions{
+			Overwrite: linkOverwrite,
+		})
+		if err != nil {
+			return err
+		}
+
+		state.CurrentProfile = profile.Name()
 		return nil
 	},
 }
@@ -22,5 +76,4 @@ var cloneCmd = &cobra.Command{
 func init() {
 	cloneCmd.Flags().BoolVarP(&link, "link", "l", false, "immediately link the profile after cloning")
 	cloneCmd.Flags().BoolVarP(&linkOverwrite, "overwrite", "o", false, "remove regular files if they exist at the link path")
-	cloneCmd.Flags().BoolVarP(&linkDryRun, "dry-run", "d", false, "print what would happen instead of creating symlinks")
 }
