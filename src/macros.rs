19d77596932f37d6959e84f7a8bbdbc2378a0f0e
Mathew Robinson <mathew@chasinglogic.io>
2023-08-25 10:48:25 +0100

    fix: Add ability to debug print and respect DFM_VERBOSE

---
+++ src/macros.rs
@@ -0,0 +1,15 @@
+use lazy_static::lazy_static;
+use std::env;
+
+lazy_static! {
+    pub static ref VERBOSITY_ENABLED: bool = env::var("DFM_VERBOSE").is_ok();
+}
+
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => {
+        if *crate::macros::VERBOSITY_ENABLED {
+            println!($($arg)*);
+        }
+    };
+}
