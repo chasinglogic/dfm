7b41bd06cf30e3917519389d506797b7be01b4b5
Mathew Robinson <mathew@chasinglogic.io>
2023-08-30 08:15:26 +0100

    fix: broken install scripts

--- scripts/local_install.sh
+++ scripts/local_install.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -x
 
 INSTALL_DIR="${INSTALL_DIR:-$HOME/.local/bin}"
 
 REPO=$(git rev-parse --show-toplevel)
 
-cd $REPO
-go build .
+cd "$REPO" || exit 1
 
-mv ./dfm "$INSTALL_DIR/"
+cargo build --release
+
+if [[ -x $(which strip) ]]; then
+    strip ./target/release/dfm
+fi
+
+mv ./target/release/dfm "$INSTALL_DIR/"
