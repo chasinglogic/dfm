c6f820f64988bfeb0450756ba0c7c762f60464ee
Mathew Robinson <mrobinson@neurolabs.ai>
2025-07-15 13:54:55 +0200

    feat: Make it so mappings with only a target_os specified simply skip all other OSes
    Closes #68

--- src/profiles/mapping.rs
+++ src/profiles/mapping.rs
@@ -1,11 +1,23 @@
+use std::fmt::Display;
+
 use log::debug;
 use regex::Regex;
 
-#[derive(Debug, Clone, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
 pub enum OS {
     Linux,
     Darwin,
     Windows,
+}
+
+impl Display for OS {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            OS::Linux => "Linux",
+            OS::Darwin => "Darwin",
+            OS::Windows => "Windows",
+        })
+    }
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
@@ -25,62 +37,66 @@ const CURRENT_OS: OS = OS::Darwin;
 const CURRENT_OS: OS = OS::Windows;
 
 impl TargetOS {
-    fn is_this_os(target: &TargetOS) -> bool {
-        match target {
-            &TargetOS::All => true,
+    fn is_this_os(&self) -> bool {
+        match self {
+            TargetOS::All => true,
             TargetOS::Vec(targets) => targets.iter().any(|t| *t == CURRENT_OS),
             TargetOS::String(desired) => *desired == CURRENT_OS,
         }
     }
-}
 
-fn default_off() -> bool {
-    false
+    fn is_specifically_this_os(&self) -> bool {
+        match self {
+            TargetOS::All => false,
+            _ => self.is_this_os(),
+        }
+    }
 }
 
 #[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
 pub struct Mapping {
     #[serde(rename = "match", with = "serde_regex")]
     term: Regex,
-    #[serde(default = "default_off")]
-    link_as_dir: bool,
-    #[serde(default = "default_off")]
-    skip: bool,
+    #[serde(default)]
+    link_as_dir: Option<bool>,
+    #[serde(default)]
+    skip: Option<bool>,
     #[serde(default)]
     target_os: TargetOS,
     #[serde(default)]
-    dest: String,
+    dest: Option<String>,
     #[serde(default)]
-    target_dir: String,
+    target_dir: Option<String>,
 }
 
 impl Mapping {
     fn new(term: &str) -> Mapping {
         Mapping {
             term: Regex::new(term).expect("Unable to compile regex!"),
-            link_as_dir: false,
-            skip: false,
+            link_as_dir: None,
+            skip: None,
             target_os: TargetOS::All,
-            dest: "".to_string(),
-            target_dir: "".to_string(),
+            dest: None,
+            target_dir: None,
         }
     }
 
     fn skip(term: &str) -> Mapping {
         let mut mapping = Mapping::new(term);
-        mapping.skip = true;
+        mapping.skip = Some(true);
         mapping
     }
 
     fn does_match(&self, path: &str) -> bool {
         let is_match = self.term.is_match(path);
         debug!("{} matches {:?} = {}", path, self.term, is_match);
+        is_match
 
-        if is_match {
-            TargetOS::is_this_os(&self.target_os)
-        } else {
-            false
-        }
+        // if is_match {
+        //     TargetOS::is_this_os(&self.target_os)
+        // } else {
+        //     false
+        // }
     }
 }
 
@@ -94,30 +110,85 @@ pub enum MapAction {
     None,
 }
 
+impl Display for MapAction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let repr = match self {
+            Self::NewDest(value) => format!("MapAction::NewDest({})", value),
+            Self::NewTargetDir(value) => format!("MapAction::NewTargetDir({})", value),
+            Self::LinkAsDir => "MapAction::LinkAsDir".to_string(),
+            Self::Skip => "MapAction::Skip".to_string(),
+            Self::None => "MapAction::None".to_string(),
+        };
+
+        f.write_str(&repr)
+    }
+}
+
 impl From<Mapping> for MapAction {
     fn from(mapping: Mapping) -> MapAction {
         MapAction::from(&mapping)
     }
 }
 
+fn check_if_skip_action(mapping: &Mapping) -> Option<MapAction> {
+    match mapping.skip {
+        Some(true) if mapping.target_os.is_this_os() => Some(MapAction::Skip),
+        Some(_) => Some(MapAction::None),
+        None => None,
+    }
+}
+
+fn check_if_dest_action(mapping: &Mapping) -> Option<MapAction> {
+    match mapping.dest {
+        Some(ref dest) if mapping.target_os.is_this_os() => {
+            Some(MapAction::NewDest(shellexpand::tilde(dest).into_owned()))
+        }
+        Some(_) => Some(MapAction::None),
+        None => None,
+    }
+}
+
+fn check_if_target_dir_action(mapping: &Mapping) -> Option<MapAction> {
+    match mapping.target_dir {
+        Some(ref target_dir) if mapping.target_os.is_this_os() => Some(MapAction::NewTargetDir(
+            shellexpand::tilde(target_dir).into_owned(),
+        )),
+        Some(_) => Some(MapAction::None),
+        None => None,
+    }
+}
+
+fn check_if_link_as_dir_action(mapping: &Mapping) -> Option<MapAction> {
+    match mapping.link_as_dir {
+        Some(true) if mapping.target_os.is_this_os() => Some(MapAction::LinkAsDir),
+        Some(_) => Some(MapAction::None),
+        None => None,
+    }
+}
+
+fn check_if_link_on_target_os_action(mapping: &Mapping) -> Option<MapAction> {
+    if !mapping.target_os.is_specifically_this_os() {
+        Some(MapAction::Skip)
+    } else {
+        None
+    }
+}
+
 impl From<&Mapping> for MapAction {
     fn from(mapping: &Mapping) -> MapAction {
-        if mapping.skip {
-            return MapAction::Skip;
-        }
+        let mapping_parsers = [
+            check_if_skip_action,
+            check_if_dest_action,
+            check_if_target_dir_action,
+            check_if_link_as_dir_action,
+            // Must come last
+            check_if_link_on_target_os_action,
+        ];
 
-        if !mapping.dest.is_empty() {
-            return MapAction::NewDest(shellexpand::tilde(mapping.dest.as_str()).into_owned());
-        }
-
-        if !mapping.target_dir.is_empty() {
-            return MapAction::NewTargetDir(
-                shellexpand::tilde(mapping.target_dir.as_str()).into_owned(),
-            );
-        }
-
-        if mapping.link_as_dir {
-            return MapAction::LinkAsDir;
+        for f in mapping_parsers {
+            if let Some(action) = f(mapping) {
+                return action;
+            }
         }
 
         MapAction::default()
@@ -167,9 +238,51 @@ impl Mapper {
 mod test {
     use super::*;
 
+    fn get_invalid_target_oses() -> Vec<OS> {
+        vec![OS::Linux, OS::Darwin, OS::Windows]
+            .into_iter()
+            .filter(|os| *os != CURRENT_OS)
+            .collect()
+    }
+
+    fn get_invalid_target_os() -> OS {
+        get_invalid_target_oses()[0].clone()
+    }
+
     #[test]
     fn test_skip_map_action_from_mapping() {
         assert_eq!(MapAction::Skip, MapAction::from(Mapping::skip("README.*")))
+    }
+
+    #[test]
+    fn test_is_not_skip_on_target_os() {
+        let config = format!(
+            r#"match: .*snippets.*
+target_os: {}"#,
+            CURRENT_OS
+        );
+        let mapping: Mapping =
+            serde_yaml::from_str(config.as_ref()).expect("invalid yaml config in test!");
+
+        assert_eq!(MapAction::None, MapAction::from(mapping))
+    }
+
+    #[test]
+    fn test_is_skip_not_on_target_os() {
+        env_logger::init();
+
+        let targets = get_invalid_target_oses();
+        for target in targets {
+            let config = format!(
+                r#"match: .*snippets.*
+target_os: {}"#,
+                target
+            );
+            let mapping: Mapping =
+                serde_yaml::from_str(config.as_ref()).expect("invalid yaml config in test!");
+
+            assert_eq!(MapAction::Skip, MapAction::from(mapping))
+        }
     }
 
     #[test]
@@ -240,5 +353,44 @@ target_dir: ~/some/subfolder"#;
             }
             _ => panic!("Reached what should be an unreachable path!"),
         }
+    }
+
+    #[test]
+    fn test_link_as_dir_map_action_from_mapping_on_wrong_os() {
+        let config = format!(
+            r#"match: .*snippets.*
+link_as_dir: true
+target_os: {}"#,
+            get_invalid_target_os()
+        );
+        let mapping: Mapping =
+            serde_yaml::from_str(config.as_ref()).expect("invalid yaml config in test!");
+        assert_eq!(MapAction::None, MapAction::from(mapping))
+    }
+
+    #[test]
+    fn test_new_dest_map_action_from_mapping_on_wrong_os() {
+        let config = format!(
+            r#"match: LICENSE
+dest: /some/new/path.txt
+target_os: {}"#,
+            get_invalid_target_os()
+        );
+        let mapping: Mapping =
+            serde_yaml::from_str(config.as_ref()).expect("invalid yaml config in test!");
+        assert_eq!(MapAction::None, MapAction::from(mapping))
+    }
+
+    #[test]
+    fn test_new_target_dir_map_action_from_mapping_on_wrong_os() {
+        let config = format!(
+            r#"match: LICENSE
+target_dir: /some/new/
+target_os: {}"#,
+            get_invalid_target_os()
+        );
+        let mapping: Mapping =
+            serde_yaml::from_str(config.as_ref()).expect("invalid yaml config in test!");
+        assert_eq!(MapAction::None, MapAction::from(mapping))
     }
 }
