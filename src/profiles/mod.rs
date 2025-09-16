4844561b3f70e26ad268fdfb0a1857870d71675e
Mathew Robinson <mrobinson@neurolabs.ai>
2025-09-16 17:10:27 +0100

    delete the old rust code

--- src/profiles/mod.rs
+++
@@ -1,6 +0,0 @@
-mod config;
-mod hooks;
-mod mapping;
-mod profile;
-
-pub use profile::Profile;
--- src/utils.rs
+++
@@ -1,7 +0,0 @@
-use std::path::PathBuf;
-use std::str::FromStr;
-
-pub fn expand_path(path: &str) -> PathBuf {
-    let expanded = shellexpand::tilde(path);
-    PathBuf::from_str(&expanded).unwrap_or_else(|_| panic!("invalid path: {}", path))
-}
