f70c5ede52a272c3257c796af0a787ccce340e19
Mathew Robinson <chasinglogic@gmail.com>
2026-03-29 10:32:22 +0100

    fix: update vendor hash

--- flake.nix
+++ flake.nix
@@ -2,17 +2,24 @@
   description = "A dotfile manager for pair programmers and lazy people";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       version = builtins.substring 0 10 self.rev or "dirty";
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
     in
     {
 
       # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
+      packages = forAllSystems (
+        system:
         let
           pkgs = nixpkgsFor.${system};
         in
@@ -24,25 +31,32 @@
             # Tests are disabled when installing because they don't work on
             # a read only file system.
             checkPhase = null;
-            vendorHash = "sha256-2xjwWrP1O2brpzNuUxG/CZtMoBiG1V5BOlsp8MSI8WQ=";
+            vendorHash = "sha256-5sHbHF3T9heFosbQPOD/MTi+F+c0KmRa/T4AOIQxW04=";
             ldflags = [
               "-X main.Version=${version}"
             ];
           };
-        });
+        }
+      );
 
       # Add dependencies that are only needed for development
-      devShells = forAllSystems (system:
+      devShells = forAllSystems (
+        system:
         let
           pkgs = nixpkgsFor.${system};
         in
         {
           default = pkgs.mkShell {
-            buildInputs = with pkgs; [ go_1_26 gopls gotools go-tools ];
+            buildInputs = with pkgs; [
+              go_1_26
+              gopls
+              gotools
+              go-tools
+            ];
           };
-        });
+        }
+      );
 
       defaultPackage = forAllSystems (system: self.packages.${system}.dfm);
     };
 }
-
